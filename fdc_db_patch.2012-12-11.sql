3263407b7e8d248d3eda484d15d574c23c962881
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2021-12-11 18:47:02 +0100

    Opdatering af industri/virksomhedsmodeller

---
+++ fdc_db_patch.2012-12-11.sql
@@ -0,0 +1,55 @@
+/* 
+-----------------------------------------------------------------------
+--   Patch 2012-12-11: Opdatering af Industri                          --
+-----------------------------------------------------------------------
+
+     search_path skal værdisættes, således at navnet på administrations schema er første parameter. 
+     Hvis der ikke er ændret på standard navn for administrationsskema "fdc_admin"
+     skal der ikke rettes i linjen
+
+*/
+SET search_path = fdc_admin, public;
+--                *********
+
+/* 
+   værdi for parameter 'f_pkey_q_comp_build' skal sættes til kolonnenavn for primærnøgle for industri/virksomheds - tabel
+   Den er som udgangspunkt sat til 'objectid'
+*/
+UPDATE parametre SET value = 'objectid' WHERE name = 'f_pkey_q_comp_build' AND parent = 'q_comp_build';
+--                            ********
+
+/* 
+   Værdi for parameter 'f_geom_q_comp_build' skal sættes til kolonnenavn for geometrifelt for industri/virksomheds - tabel
+   Den er som udgangspunkt sat til 'geom'
+*/
+UPDATE parametre SET value = 'geom' WHERE name = 'f_geom_q_comp_build' AND parent = 'q_comp_build';
+--                            ****
+
+
+-- NIX PILLE VED RESTEN....................................................................................................
+
+-- Slet model 'Industri, punkter fejlplaceret' og associerede parametre.
+DELETE FROM parametre WHERE name = 'f_mod_type_t_company' AND parent = 't_company';
+DELETE FROM parametre WHERE name = 'f_mod_date_t_company' AND parent = 't_company';
+DELETE FROM parametre WHERE name = 'f_comp_start_date_t_company' AND parent = 't_company';
+DELETE FROM parametre WHERE name = 'f_pkey_q_comp_nobuild' AND parent = 'q_comp_nobuild';
+DELETE FROM parametre WHERE name = 'f_geom_q_comp_nobuild' AND parent = 'q_comp_nobuild';
+DELETE FROM parametre WHERE name = 'q_comp_nobuild' AND parent = 'Queries';
+DELETE FROM parametre WHERE name = 'Industri, punkter fejlplaceret' AND parent = 'Industri';
+
+-- Opdatér query 'q_comp_build'
+UPDATE parametre SET value = 'SELECT DISTINCT
+  c.*,
+  o.{f_depth_t_flood}*100.00 AS vanddybde_cm,
+  b.{f_pkey_t_building} AS byg_id,
+  b.{f_muncode_t_building} AS kom_kode,
+  b.{f_usage_code_t_building} AS bbr_kode,
+  b.{f_usage_text_t_building} AS bbr_tekst
+  FROM {t_company} c 
+  LEFT JOIN {t_flood} o ON st_within(c.{f_geom_t_company},o.{f_geom_t_flood})
+  LEFT JOIN {t_building} b ON st_within(c.{f_geom_t_company},b.{f_geom_t_building})
+  WHERE o.{f_depth_t_flood} >= {Minimum vanddybde (meter)} 
+'
+WHERE name = 'q_comp_build' AND parent = 'Queries';
+
+-- Patch 2012-12-11: Opdatering af Industri slut --
