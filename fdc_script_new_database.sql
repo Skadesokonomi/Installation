a1caad9a9e35a0b04972a2eb164db6368c3bd0a8
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2021-12-12 20:31:17 +0100

    Update fdc_script_new_database.sql

--- fdc_script_new_database.sql
+++ fdc_script_new_database.sql
@@ -962,7 +962,6 @@ CREATE INDEX IF NOT EXISTS rekreative_omr_geom_idx ON fdc_data.rekreative_omr US
 
 CREATE INDEX IF NOT EXISTS vejnet_geom_idx ON fdc_data.vejnet USING gist (geom);
 
-
 /* 
 -----------------------------------------------------------------------
 --   Patch 2012-12-11: Opdatering af Industri                          --
@@ -1003,18 +1002,36 @@ DELETE FROM parametre WHERE name = 'q_comp_nobuild' AND parent = 'Queries';
 DELETE FROM parametre WHERE name = 'Industri, punkter fejlplaceret' AND parent = 'Industri';
 
 -- Opdatér query 'q_comp_build'
-UPDATE parametre SET value = 'SELECT DISTINCT
-  c.*,
-  o.{f_depth_t_flood}*100.00 AS vanddybde_cm,
-  b.{f_pkey_t_building} AS byg_id,
-  b.{f_muncode_t_building} AS kom_kode,
-  b.{f_usage_code_t_building} AS bbr_kode,
-  b.{f_usage_text_t_building} AS bbr_tekst
-  FROM {t_company} c 
-  LEFT JOIN {t_flood} o ON st_within(c.{f_geom_t_company},o.{f_geom_t_flood})
-  LEFT JOIN {t_building} b ON st_within(c.{f_geom_t_company},b.{f_geom_t_building})
-  WHERE o.{f_depth_t_flood} >= {Minimum vanddybde (meter)} 
+UPDATE parametre SET value = 'WITH cte1 AS (
+  SELECT DISTINCT ON (c.{f_pkey_t_company})
+    c.*,
+    o.{f_depth_t_flood}*100.00 AS vanddybde_cm,
+    b.{f_pkey_t_building} AS byg_id,
+    b.{f_muncode_t_building} AS kom_kode,
+    b.{f_usage_code_t_building} AS bbr_kode,
+    b.{f_usage_text_t_building} AS bbr_tekst
+    FROM {t_company} c 
+    LEFT JOIN {t_building} b ON st_within(c.{f_geom_t_company},b.{f_geom_t_building})
+    LEFT JOIN {t_flood} o ON st_intersects(b.{f_geom_t_building},o.{f_geom_t_flood})
+    WHERE o.{f_depth_t_flood} >= {Minimum vanddybde (meter)} AND b.{f_pkey_t_building} IS NOT NULL 
+  ORDER BY c.{f_pkey_t_company} ASC, o.{f_depth_t_flood} DESC
+),
+cte2 AS (
+  SELECT DISTINCT
+    c.*,
+    o.{f_depth_t_flood}*100.00 AS vanddybde_cm,
+    b.{f_pkey_t_building} AS byg_id,
+    b.{f_muncode_t_building} AS kom_kode,
+    b.{f_usage_code_t_building} AS bbr_kode,
+    b.{f_usage_text_t_building} AS bbr_tekst
+    FROM {t_company} c 
+    LEFT JOIN {t_flood} o ON st_within(c.{f_geom_t_company},o.{f_geom_t_flood})
+    LEFT JOIN {t_building} b ON st_within(c.{f_geom_t_company},b.{f_geom_t_building})
+    WHERE o.{f_depth_t_flood} >= {Minimum vanddybde (meter)} AND b.{f_pkey_t_building} IS NULL 
+) 
+SELECT * FROM cte1 UNION ALL SELECT * FROM cte2
 '
 WHERE name = 'q_comp_build' AND parent = 'Queries';
 
 -- Patch 2012-12-11: Opdatering af Industri slut --
+
