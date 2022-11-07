c6bd90d92bfe55de64df07adc77740edbdab0aac
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2022-11-07 15:19:34 +0100

    Update fdc_db_patch.2022-05-17.sql

--- patches/fdc_db_patch.2022-05-17.sql
+++ patches/fdc_db_patch.2022-05-17.sql
@@ -17,7 +17,7 @@ INSERT INTO skadefunktioner (skade_type, skade_kategori, b0, b1, b2, c0) VALUES 
 INSERT INTO skadefunktioner (skade_type, skade_kategori, b0, b1, b2, c0) VALUES ('Skybrud',   'Erhverv_lav', 0,      0,  351.63, 578);
 INSERT INTO skadefunktioner (skade_type, skade_kategori, b0, b1, b2, c0) VALUES ('Vandløb',   'Erhverv_lav', 0, 115.66,  -73.48, 578);
 
-UPDATE bbr_anvendelse SET skade_kategori = 'Erhverv_lav' WHERE bbr_anv_kode IN (211, 212, 213, 214, 215, 216, 218, 310, 313, 314, 319, 323, 534, 535)
+UPDATE bbr_anvendelse SET skade_kategori = 'Erhverv_lav' WHERE bbr_anv_kode IN (211, 212, 213, 214, 215, 216, 218, 310, 313, 314, 319, 323, 534, 535);
 
 --   Patch 2022-05-17: Indførsel af ny skadekategori "Erhverv_lav" slut
 
